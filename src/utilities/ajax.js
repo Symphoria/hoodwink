9271bba98c5c3cc6ee5da9f06a86ac3024aac470
Harshit Jain <hj.harshit007@gmail.com>
2017-12-23 19:14:14 +0530

    Changed API url to deployed one

--- src/utilities/ajax.js
+++ src/utilities/ajax.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://hoodwink-api.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json'
   }
